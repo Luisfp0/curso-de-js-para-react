5063545f9e9ae0aad5d5a7730b1f865843fa90f3
Luisfp0 <luisfernandodepaulosousa0020@gmail.com>
2023-04-07 17:16:02 -0300

    git add

---
+++ src/components/CardGame/main.js
@@ -0,0 +1,25 @@
+import './style.css';
+
+function CardGame() {
+    return `
+    <article class="card-game">
+      <img src="Images/alura-pixel 1.png" alt="Logo da alura">
+    </article>
+    <article class="card-game">
+      <img src="Images/alura-pixel 1.png" alt="Logo da alura">
+    </article>
+    <article class="card-game">
+      <img src="Images/alura-pixel 1.png" alt="Logo da alura">
+    </article>
+    <article class="card-game">
+      <img src="Images/alura-pixel 1.png" alt="Logo da alura">
+    </article>
+    <article class="card-game">
+      <img src="Images/alura-pixel 1.png" alt="Logo da alura">
+    </article>
+    <article class="card-game">
+      <img src="Images/alura-pixel 1.png" alt="Logo da alura">
+    `;
+}
+
+export default CardGame;
---
+++ src/components/Player/main.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+function player() {
+    return `
+      <article class="playerName">Player 1</article>
+      <article class="playerName">Player 2</article>
+      `;
+}
+
+export default player;
