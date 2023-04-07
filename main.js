3dbda93f6334f48400851a61a305d8494d65aa02
Luisfp0 <luisfernandodepaulosousa0020@gmail.com>
2023-04-07 17:15:42 -0300

    Adicionado player

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import './src/styles/settings/colors.css';
 import './src/styles/generic/reset.css';
-import CardGame from './src/components/CardGame';
+import CardGame from './src/components/CardGame/main';
+import player from './src/components/Player/main';
 
 import './src/styles/elements/base.css';
 
 const $root = document.querySelector('#root');
 const $htmlCardGame = CardGame();
+const $htmlplayer = player();
 $root.insertAdjacentHTML('beforeend', $htmlCardGame);
+$root.insertAdjacentHTML('beforeend', $htmlplayer);
