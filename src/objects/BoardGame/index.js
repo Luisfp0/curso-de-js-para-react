0bea6181e0b8a67311fe883668fd987f9d683bee
Luisfp0 <luisfernandodepaulosousa0020@gmail.com>
2023-04-10 11:05:55 -0300

    Feito o CSS do BoardGame

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -3,11 +3,10 @@ import CardGame from '../../components/CardGame/main.js';
 
 function BoardGame(amountCards) {
     const $htmlCardGame = CardGame();
-    const $htmlBoardGame = $htmlCardGame.repeat(amountCards);
+    const $htmlContent = $htmlCardGame.repeat(amountCards);
 
-    console.log($htmlBoardGame);
-
-    return $htmlBoardGame;
+    return `<section class="board-game">
+        ${$htmlContent}
+    </section>`;
 }
-
 export default BoardGame;
