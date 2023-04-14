ebd4315b74064ac3967b32d2a78879726b4c90f0
Luisfp0 <luisfernandodepaulosousa0020@gmail.com>
2023-04-14 18:50:46 -0300

    Problema de comparação entre os cards resolvido

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,7 +1,6 @@
 import './style.css';
 import CardFrontBack from '../../components/CardFrontBack';
 import cards from './data.js';
-import CardGame from '../../components/CardGame/main';
 
 function BoardGame() {
   const flipAndHideCards = ($cardsActive) => {
@@ -12,26 +11,37 @@ function BoardGame() {
     const currentPlayer = $arrowDown.getAttribute('data-currentplayer');
     $arrowDown.setAttribute('data-currentplayer', currentPlayer == 1 ? 2 : 1);
   }
+  let clickScreen = 0;
+  let click1 = 0;
+  let click2 = 0;
 
   window.boardGame = {};
   window.boardGame.handleClick = (event) => {
+    console.log(clickScreen)
     const target = event.target;
     const $cardFrontBack = target.closest('.card-front-back');
     const imgCardBack = $cardFrontBack.querySelector('.card.-back .card-game .img-back');
-    console.log(imgCardBack)
-
-
+    if (clickScreen === 0) {
+      click1 = imgCardBack;
+      clickScreen++;
+      console.log("Esse é o click 1", click1)
+    } else {
+      click2 = imgCardBack;
+      console.log("Esse é o click 2", click2)
+      clickScreen = 0;
+    }
     const $boardGame = document.querySelector('.board-game');
     const $arrowDown = document.querySelector('.arrow-down');
     const $cardsActive = $boardGame.querySelectorAll('.card-front-back.-active');
-    if ($cardsActive.length === 2) {
-      if (imgCardBack) {
-        console.log('talvez')
-      }
-      setTimeout(() => {
-        flipAndHideCards($cardsActive);
-        changePlayer($arrowDown);
-      }, 1000);
+    if ($cardsActive.length >= 2) {
+      if (click1.src === click2.src) {
+        console.log('Deu certo');
+      } else {
+        setTimeout(() => {
+          flipAndHideCards($cardsActive);
+          changePlayer($arrowDown);
+        }, 1000)
+      };
     };
   }
   const htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.altIcon));
