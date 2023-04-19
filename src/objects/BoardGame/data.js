6bb1da511636533f60ebde4877a85262dc076f22
Luisfp0 <luisfernandodepaulosousa0020@gmail.com>
2023-04-19 14:37:08 -0300

    Finalizado o embaralhamento das cartas

--- src/objects/BoardGame/data.js
+++ src/objects/BoardGame/data.js
@@ -3,24 +3,24 @@ const cards = [{
   altIcon: 'Logo do html'
 },
 {
+  icon: 'css',
+  altIcon: 'Logo do css'
+},
+{
+  icon: 'css',
+  altIcon: 'Logo do css'
+},
+{
+  icon: 'javascript',
+  altIcon: 'Logo do javascript'
+},
+{
+  icon: 'javascript',
+  altIcon: 'Logo do javascript'
+},
+{
   icon: 'html',
   altIcon: 'Logo do html'
-},
-{
-  icon: 'css',
-  altIcon: 'Logo do css'
-},
-{
-  icon: 'css',
-  altIcon: 'Logo do css'
-},
-{
-  icon: 'javascript',
-  altIcon: 'Logo do javascript'
-},
-{
-  icon: 'javascript',
-  altIcon: 'Logo do javascript'
 }];
 
 export default cards;
